ef5d73fb5fcf6402e66e5f671b4bdeddbd30406d
flmngwllm <flmngwllm232@gmail.com>
2024-08-17 01:51:52 -0400

    adjusted the www record to correct name

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.${var.DOMAIN_NAME}.com"
+  name    = "www.${var.DOMAIN_NAME}"
   type    = "CNAME"
   ttl     = 300
   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
