e6bcfcb48493a353e77c306955a76fdf7db2ac9f
flmngwllm <flmngwllm232@gmail.com>
2024-08-16 22:54:36 -0400

    corrected casing for var bucket name

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,7 +1,7 @@
 
 resource "aws_cloudfront_origin_access_control" "S3_OAC" {
   name              = "s3_static-oac"
-  description       = "OAC for S3 bucket ${var.bucket_name}"
+  description       = "OAC for S3 bucket ${var.BUCKET_NAME}"
   origin_access_control_origin_type = "s3"
   signing_behavior  = "always"
   signing_protocol  = "sigv4"
