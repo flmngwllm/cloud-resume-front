3279199a4280770a2d8958f0fe56e6ab8d5a7743
flmngwllm <flmngwllm232@gmail.com>
2024-08-16 22:39:40 -0400

    fixed aws_route53_record field

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -9,7 +9,7 @@ resource "aws_acm_certificate" "acm_certificate" {
 
 resource "aws_route53_record" "acm_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
