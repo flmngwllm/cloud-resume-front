db9b9e349242fa136bca95dda7e26b3b749ed3ec
flmngwllm <flmngwllm232@gmail.com>
2024-08-22 17:43:55 -0400

    added a bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -38,3 +38,20 @@ resource "aws_s3_bucket_website_configuration" "s3_website" {
 
 }
 
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket_name.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.bucket_name.arn}/*"
+        Principal = {
+          AWS = "*"
+        }
+      }
+    ]
+  })
+}
