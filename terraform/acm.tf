a86d5beb005595d52226e6cb587827c0d7cb3d71
flmngwllm <flmngwllm232@gmail.com>
2025-03-26 20:46:44 -0400

    fix: add missing newline in ACM validation resource

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -15,6 +15,7 @@ resource "aws_route53_record" "acm_validation" {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
+      
     }
   }
 
